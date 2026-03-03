5a3810f1e8654e99ddad1a6c7ddf07891467d6e9
Keith Ballard <michael.ballard.28@afrl.af.mil>
2026-03-03 16:23:05 -0500

    feat: improved 1D test for multipoint constraints.

--- tests/test_periodic_1d_elastic.py
+++ tests/test_periodic_1d_elastic.py
@@ -31,19 +31,6 @@ Q = get_quadrature(fe_type=fe_type)[0].shape[0]  # number of quadrature points
 
 print("# DoFs = ", F)
 
-# Periodic boundary conditions
-mpcs = [
-    MultiPointConstraint(dep_dof=F - 1, indep_dofs=[0], factors=[1.0], rhs_constant=0.1)
-]
-
-dirichlet_constraints = [
-    DirichletConstraint(dep_dof=1, value=0.05),
-    # DirichletConstraint(dep_dof=2, value=0.1),
-]
-
-print(mpcs)
-
-
 # Set material properties
 matrix_mat_params = jnp.array([1.0e9])  # E
 element_batches = [
@@ -56,31 +43,73 @@ element_batches = [
     )
 ]
 
-# Solve the boundary value problem
-u, residual, element_batches = solve_bvp(
-    element_residual_func=linear_elasticity_residual,
-    vertices_vd=points,
-    element_batches=element_batches,
-    dirichlet_bcs=dirichlet_constraints,
-    multipoint_constraints=mpcs,
-    solver_options=SolverOptions(
-        linear_solve_type=LinearSolverType.CG_JAX_SCIPY_W_INFO,
-        # linear_precond_type=PreconditionerType.JACOBI,
-        # linear_solve_type=LinearSolverType.SPSOLVE_PYPARDISO,
-        nonlinear_max_iter=1,
-        linear_max_iter=5,
-    ),
-    plot_convergence=True,
-)
-print("|R| = ", jnp.linalg.norm(residual))
-# print(residual)
+# Sets of boundary conditions to test
+mpcs = [
+    [
+        MultiPointConstraint(
+            dep_dof=F - 1, indep_dofs=[0], factors=[1.0], rhs_constant=0.1
+        )
+    ],  # Case 1
+    [
+        MultiPointConstraint(
+            dep_dof=F - 1, indep_dofs=[0], factors=[1.0], rhs_constant=0.1
+        )
+    ],  # Case 2
+    [
+        MultiPointConstraint(
+            dep_dof=F - 1, indep_dofs=[0], factors=[1.0], rhs_constant=0.1
+        )
+    ],  # Case 3
+]
+dirichlet_constraints = [
+    [
+        DirichletConstraint(dep_dof=1, value=0.05),
+    ],  # Case 1
+    [
+        DirichletConstraint(dep_dof=1, value=0.05),
+        DirichletConstraint(dep_dof=2, value=0.1),
+    ],  # Case 2
+    [
+        DirichletConstraint(dep_dof=0, value=0.05),
+    ],  # Case 3
+]
 
-print(f"u = {u}")
 
-# Write output
-mesh = meshio.Mesh(
-    points,
-    [("line", cells)],
-    point_data={"u": u.reshape((points.shape[0], U))},
-)
-mesh.write(get_output(Path(__file__).stem + ".vtk"))
+for i, (mc, dc) in enumerate(zip(mpcs, dirichlet_constraints)):
+    print(f"\n************\nCase {i}\n")
+    print(f"Multipoint constraints:\n{mc}")
+    print(f"Dirichlet constraints:\n{dc}")
+
+    # Solve the boundary value problem
+    u, residual, element_batches = solve_bvp(
+        element_residual_func=linear_elasticity_residual,
+        vertices_vd=points,
+        element_batches=element_batches,
+        dirichlet_bcs=dc,
+        multipoint_constraints=mc,
+        solver_options=SolverOptions(
+            linear_solve_type=LinearSolverType.CG_JAX_SCIPY_W_INFO,
+            # linear_precond_type=PreconditionerType.JACOBI,
+            # linear_solve_type=LinearSolverType.SPSOLVE_PYPARDISO,
+            nonlinear_max_iter=1,
+            linear_max_iter=5,
+        ),
+        plot_convergence=True,
+    )
+    print("|R| = ", jnp.linalg.norm(residual))
+    print(f"u = {u}")
+
+    # Ensure multipoint constraints are satisified
+    for c in mc:
+        assert (
+            abs(
+                u[c.dep_dof]
+                - sum([factor * u[dof] for dof, factor in c.indep_dof_terms.items()])
+                - c.rhs_constant
+            )
+            < 1e-6
+        ), f"MPC {c} is not satisfied"
+
+    # Ensure Dirichlet BCs are satisfied
+    for c in dc:
+        assert abs(u[c.dep_dof] - c.value) < 1e-6, f"Dirichlet {c} is not satisfied"
