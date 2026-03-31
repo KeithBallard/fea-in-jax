35d481cdc2eafd9521e08be3c309d6f83c982f68
Cattaneo123 <bubu.cattaneo@gmail.com>
2026-03-31 11:53:34 -0600

    Better error catching and memory handling model

--- tests/test_mpi4py_cuda/test_mpi4py_cuda_jax-petsc/test_mpi4py_cuda_jax-petsc_11.py
+++ tests/test_mpi4py_cuda/test_mpi4py_cuda_jax-petsc/test_mpi4py_cuda_jax-petsc_11.py
@@ -1,10 +1,13 @@
 from mpi4py import MPI
 import mpi4jax
-import jax
-import jax.numpy as jnp
+
 import petsc4py
 import cupy as cp
 import ctypes as ct
+import sys
+import traceback
+import os
+import signal
 
 # 1. Initialize MPI communicator via mpi4py
 comm = MPI.COMM_WORLD
@@ -14,6 +17,12 @@ size = comm.Get_size()
 petsc4py.init(comm=comm)
 from petsc4py import PETSc
 
+
+os.environ['XLA_PYTHON_CLIENT_PREALLOCATE']="false" #needed to avoid JAX eating all the memory MPI will later need. Probably there's an intelligent way of doing this, but I'm not sure quite what it is yet, y'dig?
+
+
+import jax
+import jax.numpy as jnp
 jax.config.update('jax_enable_x64', True)
 
 
@@ -23,7 +32,7 @@ MatSetValuesCOO.restype = ct.c_int  # PetscErrorCode is just a C `int` in terms 
 MatSetValuesCOO.argtypes = [ct.c_void_p, ct.c_void_p, ct.c_int] # [Mat, PetscScalar*, InsertMode], I'm using void* instead of PetscScalar* for simplicy, could use `ct.POINTER(ct.c_{float|double})` instead.
 
 
-arraySize = 10
+arraySize = 1000000
 
 @jax.jit
 def parallel_jaxCreation():
@@ -54,7 +63,7 @@ def parallelBuildVec(arr):
 
     local_arr = GPUPointerArray[start:end]
     #createWithDLPack assumes the input array is already the local portion of the distributed vector. It does zero redistribution.
-    arr_petsc = PETSc.Vec().createWithDLPack( #
+    arr_petsc = PETSc.Vec().createWithDLPack( 
         local_arr,
         size=arrsize,
         comm=comm
@@ -70,7 +79,7 @@ def parallelBuildMat(data,rows,cols):
     GPUdata = cp.from_dlpack(data,copy=False)
     
     if rank != 0:
-        GPUdata = cp.from_dlpack(jnp.zeros((arraySize,1),dtype=jnp.float64),copy=False) #this needs to be here as it adds despite my telling it to insert
+        GPUdata = cp.from_dlpack(jnp.zeros((arraySize,1),dtype=jnp.float64),copy=False) #this needs to be here as it adds despite my telling it to insert. There's probably a fix somewhere involving a similar breakdown to vec creation
     
     GPUrows = cp.from_dlpack(rows,copy=False)
     GPUcols = cp.from_dlpack(cols,copy=False)
@@ -105,20 +114,29 @@ def parallelMatVec(mat,vec):
     ksp.getPC().setType("none")
 
     ksp.solve(vec,outputVec)
-    outputVec.view()
+    #outputVec.view()
 
-vals = parallel_jaxCreation()
-vals = parallel_jaxOneRankChange(vals)
+try:
+    vals = parallel_jaxCreation()
+    vals = parallel_jaxOneRankChange(vals)
 
-rowcols = jnp.arange(arraySize,dtype=jnp.int64)
+    rowcols = jnp.arange(arraySize,dtype=jnp.int64)
 
-mat = parallelBuildMat(vals,rowcols,rowcols)
+    mat = parallelBuildMat(vals,rowcols,rowcols)
 
+        
+    vec = parallel_jaxCreation()
+    vec = parallelBuildVec(vec)
+
+    parallelMatVec(mat,vec)
+    print("reached")
+except Exception as e:  
+    if rank==0:
+        print("exception",e)
+        traceback.print_exc()
     
-vec = parallel_jaxCreation()
-vec = parallelBuildVec(vec)
-
-parallelMatVec(mat,vec)
+    comm.Abort(1)
+    os.kill(os.getpid(),signal.SIGKILL) #this is essential as otherwise python will hold on to memory it's not supposed to and force the user (you) to call to sudo pkill -f python3.12
 
 
 
