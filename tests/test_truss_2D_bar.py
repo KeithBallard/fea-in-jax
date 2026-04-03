ac0913c7431ed7e9b778aa4044a9a7436aec8cae
Nathan Willis <46230036+nathan-willis@users.noreply.github.com>
2026-04-03 16:18:44 -0600

    Created test_truss_2D_par.py to test linear_truss_residual and elastic_truss in 2D. Currently FAILING. I suspect det_J_q and dphi_dx_qnd are wrong.

---
+++ tests/test_truss_2D_bar.py
@@ -0,0 +1,113 @@
+from helper import *
+import matplotlib.pyplot as plt
+
+jax.config.update("jax_enable_x64", True)
+
+import jax.extend
+
+print(jax.extend.backend.get_backend().platform)
+
+# from jax_smi import initialise_tracking
+# initialise_tracking()
+
+x_of_t = lambda t:1*t
+y_of_t = lambda t:0*t
+n_elements = 4
+t_soln = np.linspace(0,1.2,n_elements+1)
+x_soln = x_of_t(t_soln)
+y_soln = y_of_t(t_soln)
+
+t = np.linspace(0,1,n_elements+1,dtype=np.float32)
+x = x_of_t(t)
+y = y_of_t(t)
+points=np.vstack((x,y)).T
+cells = np.array([[i, i + 1] for i in range(len(points) - 1)], dtype=np.uint64)
+# points = np.linspace(0, 1, n_elements + 1, dtype=np.float32).reshape((-1, 1))
+cell_domain_ids = np.zeros(cells.shape[0], dtype=np.int64)
+
+# Sizes of arrays
+U = 2  # number of solution components
+V = points.shape[0]  # number of vertices
+E = cells.shape[0]  # number of elements
+F = V * U  # number of DoFs
+fe_type = FiniteElementType(
+    cell_type=CellType.interval,
+    family=ElementFamily.P,
+    basis_degree=2,
+    lagrange_variant=LagrangeVariant.equispaced,
+    quadrature_type=QuadratureType.default,
+    quadrature_degree=2,
+)
+Q = get_quadrature(fe_type=fe_type)[0].shape[0]  # number of quadrature points
+
+print("# DoFs = ", F)
+
+# Set material properties
+matrix_mat_params = jnp.array([1e9])  # E
+
+# Set boundary conditions. Leave the (0,0) end point fixed, but take (2,1)->(2.4,1.2)
+# The displacement is in the direciton of the bar, so this should be the same as a 1D displacement.
+bcs = (
+    [
+        DirichletBC(bc_type = BCType.NODE,component=0, index=0,value=0.0),
+        DirichletBC(bc_type = BCType.NODE,component=1, index=0,value=0.0),
+        DirichletBC(bc_type=BCType.NODE,component=0,index=n_elements,value=x_soln[-1]),
+        DirichletBC(bc_type=BCType.NODE,component=1,index=n_elements,value=y_soln[-1])
+    ]
+)
+
+# Example using the truss elements
+element_batches_truss = [
+    ElementBatch(
+        fe_type=fe_type,
+        n_dofs_per_basis=2,
+        connectivity_en=cells,
+        constitutive_model=elastic_truss,
+        material_params=matrix_mat_params,
+    )
+]
+
+u_truss, residual_truss, element_batches_truss = solve_bvp(
+    element_residual_func=linear_truss_residual,
+    vertices_vd=points,
+    element_batches=element_batches_truss,
+    boundary_conditions=bcs,
+    solver_options=SolverOptions(
+        linear_solve_type=LinearSolverType.CG_JAX_SCIPY_W_INFO,
+        # linear_precond_type=PreconditionerType.JACOBI,
+        # linear_solve_type=LinearSolverType.SPSOLVE_PYPARDISO,
+        nonlinear_max_iter=1,
+        linear_max_iter=5,
+    ),
+    plot_convergence=False,
+)
+
+u_truss = u_truss.reshape((-1,2))
+print("\n*** Truss Eleemnts! ***")
+print("|R| = ", jnp.linalg.norm(residual_truss))
+print('-'*45)
+print(f"{'initial':^19}|{'final':^24}")
+print('-'*45)
+for x, v in zip(points, u_truss):
+    # Format each coordinate and value to 6 decimal places
+    coord_str = "[" + " ".join(f"{xi:7.3f}" for xi in x) + "]"
+    val_str = "[" + " ".join(f"{vi: .3e}" for vi in v) + "]"
+    print(f"{coord_str:>18} | {val_str:>24}")
+print("\n"*2)
+
+# Check solution against Dirichlet boundary conditions
+dirichlet_dofs = np.array([bc.index for bc in bcs])
+dirichlet_values = np.array([bc.value for bc in bcs])
+dirichlet_comp = np.array([bc.component for bc in bcs])
+assert jnp.isclose(u_truss[dirichlet_dofs,dirichlet_comp].reshape((-1,2)), dirichlet_values.reshape((-1,2))).all(), f"Dirichlet is not satisfied"
+print("Woo Hoo! Solution at least matches at the endopints\n")
+
+plt.scatter(*points.T,label = 'initial')
+plt.scatter(*u_truss.T,marker='d',label = 'solution')
+plt.scatter(x_soln,y_soln,marker='x',label = 'truth')
+plt.legend()
+plt.show()
+
+# Check solutions against "known" solution. 
+assert jnp.isclose(u_truss,np.vstack((x_soln,y_soln)).T).all(), "does not match expected solution!" 
+
