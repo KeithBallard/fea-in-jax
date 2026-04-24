e5b00e806cebd19ed47e8b0c0008fd3907eeacdb
Nathan Willis <46230036+nathan-willis@users.noreply.github.com>
2026-04-24 13:47:27 -0600

    Made some simple Neumann test cases.

--- tests/run_truss_tests.sh
+++ tests/run_truss_tests.sh
@@ -2,6 +2,7 @@
 pytest \
   test_truss_1D.py \
   test_truss_2D_bar.py \
-  test_truss_2D_bridge.py \
+  test_Neumann_truss_2D_bridge.py \
+  test_Neumann_truss_2element.py \
   test_truss_3D_bar.py \
   -q
