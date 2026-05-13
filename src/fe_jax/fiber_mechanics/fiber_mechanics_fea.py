150ce986dc2189953d76b1acdc85fb4a44285afe
Nathan Willis <nwillis@ucmerced.edu>
2026-05-13 15:53:12 -0600

    fiber mechanics updates
    fix: added [bundle_i] to VTMSBundle methods as necessary to correctly
    mirror the structure of VTMSFabric to work with vtk_exporter.
    
    feat: added solver_options as an argument to solve_fiber_mechanics_bvp
    to thread to solve_bvp.

--- src/fe_jax/fiber_mechanics/fiber_mechanics_fea.py
+++ src/fe_jax/fiber_mechanics/fiber_mechanics_fea.py
@@ -16,6 +16,7 @@ def solve_fiber_mechanics_bvp(
     materials: list[VTMSFiberMaterial],
     boundary_conditions: List[DirichletBC | NeumannBC | PeriodicBC],
     contact_search_radius: float,
+    solver_options: SolverOptions,
 ):
     """
     TODO document
@@ -73,7 +74,7 @@ def solve_fiber_mechanics_bvp(
     contact_fe_type = fe_type
     self_adjacency_block = 3
     contact_search_radius = contact_search_radius
-    contact_params = jnp.array([10 * np.max(material_params[:,0]), 1, contact_search_radius])  # E_max, A, R
+    contact_params = jnp.array([10 * np.max(material_params[:,0]), np.max(material_params[:,1]), contact_search_radius])  # E_max, A, R
 
     def contact_pair_generator() -> list[ElementBatch]:
         contact_cells = contact_batch(
@@ -97,15 +98,9 @@ def solve_fiber_mechanics_bvp(
         vertices_vd=fabric.points,
         element_batches=element_batches,
         boundary_conditions=boundary_conditions,
-        solver_options=SolverOptions(
-            linear_solve_type=LinearSolverType.CG_JAX_SCIPY_W_INFO,
-            # linear_precond_type=PreconditionerType.JACOBI,
-            # linear_solve_type=LinearSolverType.SPSOLVE_PYPARDISO,
-            nonlinear_max_iter=50,
-            linear_max_iter=50,
-        ),
+        solver_options=solver_options,
         plot_convergence=False,
         contact_batch_generator=contact_pair_generator,
     )
 
-    return u_truss
+    return u_truss, residual_truss, element_batches_truss
