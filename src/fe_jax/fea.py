0d2d593c576c3d07f4255659bf4ae1e1e8438248
Nathan Willis <nwillis@ucmerced.edu>
2026-05-18 15:28:33 -0600

    fix: minor edits to fea.py and fiber_mechanics_bcp.py

--- src/fe_jax/fea.py
+++ src/fe_jax/fea.py
@@ -730,7 +730,6 @@ def solve_nonlinear_step(
         )
 
         u_f = u_f + delta_u
-        # u_f = constraints.apply_to_solution(u_f)
         R_f, new_internal_state_beqi = residual_isv_func_w_constraints(u_f=u_f)
 
         return (
